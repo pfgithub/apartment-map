ca62dcb7488d75d3b00bf61c7e7a80886bc5ef80
pfg <pfg@pfg.pw>
2024-06-19 15:36:54 -0500

    process genbook

--- src/process.ts
+++ src/process.ts
@@ -69,3 +69,83 @@ console.log("One way connections ("+one_ways.size+"):");
 for(const mb of one_ways.values()) console.log("- "+mb);
 console.log("Missing Contents ("+missing_content.size+"):");
 for(const link of missing_content.values()) console.log("- "+link);
+
+// cmdgen
+type Color = "dark_blue" | "red";
+type ClickEvent = {action: "change_page", value: `${number}`};
+type HoverEvent = {action: "show_text", contents: string};
+type TextItm = {
+    text: string,
+    underlined?: boolean,
+    color?: Color,
+    clickEvent?: ClickEvent,
+    hoverEvent?: HoverEvent,
+};
+type Text = string | TextItm;
+type BookLine = Text[];
+type BookPage = BookLine[];
+const bookpages: BookPage[] = [
+    [["", {text: "a", underlined:true,color:"dark_blue", clickEvent: {
+        action: "change_page",
+        value: "2",
+    }, hoverEvent: {
+        action: "show_text",
+        contents: "Board Area",
+    }}]],
+];
+function addtext(line: BookLine) {
+    const lastpage = bookpages[bookpages.length - 1];
+    lastpage.push(line);
+}
+function pagebreak() {
+    bookpages.push([]);
+}
+let link_values: {ce: TextItm, link: string}[] = [];
+const page_results = new Map<string, number>();
+for(const [self_name, place] of places.entries()) {
+    pagebreak();
+    addtext([self_name]);
+    for(const link of place.links) {
+        const ce: TextItm = {
+            text: link.place_name,
+            color: "dark_blue",
+            // hoverEvent: {
+            //     action: "show_text",
+            //     contents: link.place_name,
+            // },
+        };
+        link_values.push({ce, link: link.place_name});
+        addtext(["\n-> ",ce]);
+    }
+    page_results.set(self_name, bookpages.length);
+}
+for(const link_value of link_values) {
+    const resnum = page_results.get(link_value.link);
+    if(resnum != null) {
+        link_value.ce.clickEvent = {
+            action: "change_page",
+            value: `${resnum}`,
+        };
+        link_value.ce.underlined = true;
+    }else{
+        link_value.ce.color = "red";
+    }
+}
+
+let rescmd = "/give @p written_book[written_book_content={";
+rescmd += "pages:[";
+for(const [i, page] of bookpages.entries()) {
+    if(i !== 0) rescmd += ",";
+    rescmd += JSON.stringify(JSON.stringify(page));
+}
+rescmd += "]";
+rescmd += ",title:"+JSON.stringify("Find Your Way™");
+rescmd += ",author:"+JSON.stringify("NovaWay");
+rescmd += "}]";
+if((false)) {
+    if(rescmd.length > 32500) {
+        console.log("rescmd too long:", rescmd.length);
+    }else{
+        console.log(rescmd);
+    }
+}
