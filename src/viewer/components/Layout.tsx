9e07ba6250f965788f771d08afe63f12ddf7b7b7
pfg <pfg@pfg.pw>
2025-05-24 15:30:55 -0700

    srclnk

--- src/viewer/components/Layout.tsx
+++ src/viewer/components/Layout.tsx
@@ -17,8 +17,8 @@ const Layout: React.FC = () => {
       </div>
       <footer className="bg-gray-800 text-gray-300 text-center p-6">
         <div className="container mx-auto">
-          <p>Brought to you by NovaWays™</p>
-          <p className="text-sm mt-1">Find Your Way™</p>
+          <p>Brought to you by NovaWays™: Find Your Way™</p>
+          <p><a href="/viewerprompt.txt" className="underline">Source Code</a></p>
         </div>
       </footer>
     </div>
