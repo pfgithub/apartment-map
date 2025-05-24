487d03b31ec4170e937ad897c63714011a85fdff
pfg <pfg@pfg.pw>
2025-05-23 23:21:36 -0700

    notes

--- src/planner/api.ts
+++ src/planner/api.ts
@@ -1,36 +1,65 @@
+export type BuildingID = string & {__is_building_id: true};
+export type HallID = string & {__is_hall_id: true};
+export type ConnectionID = string & {__is_connection_id: true};
+export type RoomID = string & {__is_room_id: true};
+export type Root = {
+    buildings: Record<BuildingID, Building>,
+    halls: Record<HallID, Hall>,
+    connections: Record<ConnectionID, Connection>,
+    rooms: Record<RoomID, Room>,
+};
 export type Building = {
-    id: string;
+    id: BuildingID,
     label: string;
     relations: {
-        halls: Hall[],
+        halls: HallID[],
     }
 };
 export type Hall = {
-    id: string,
+    id: HallID,
     name: string,
     image: Image,
     description: string,
 
     relations: {
-        rooms: Room[],
-        connections: Connection[],
-        building: Building,
+        rooms: RoomID[],
+        connections: ConnectionID[],
+        building: BuildingID[],
     },
 };
 export type Connection = {
+    id: ConnectionID,
     seconds: number,
     relations: {
-        from: Hall,
-        to: Hall,
+        from: HallID,
+        to: HallID,
     }
 };
 export type Room = {
-    id: string,
+    id: RoomID,
     name: string,
+    price: number,
+    available: boolean,
     image: Image,
 
     relations: {
-        hall: Hall,
+        hall: HallID,
     },
 };
 export type Image = {url: string, width: number, height: number};
+
+/*
+Create a web application to browse halls and rooms. Fetch the data from /root.json. Use tailwind CSS for styling.
+
+The app should have:
+
+    A homepage where you can see all the available rooms
+
+    Individual room pages for each room
+
+    Individual hall pages for each hall
+
+    Search to find any room or hall
+
+    Navigation to get directions from any hall to a different hall
+*/
